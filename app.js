9868beb8e02232bfc1de4073d233a1827133aeb7
Chris_Chan <chrischandfw@gmail.com>
2022-03-01 19:13:46 -0600

    eslint errors fixed

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ showButton.addEventListener('click', () => {
     animalPic.classList.toggle('hidden');
 });
 
-console.log(showButton, animal, 'validate grabbed')
+console.log(showButton, animal, 'validate grabbed');
 
-console.log('show button clicked', 'validate clicked')
+console.log('show button clicked', 'validate clicked');
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
